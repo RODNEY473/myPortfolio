d1ea452f4571b936ac92fe261cc14ff544a6046b
RODNEY473 <sphiwekhubayi34@gmail.com>
2025-08-02 00:03:08 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Typed JS 
-
-const typed = new Typed(".multiple-text", {
-  strings: ["Fronted Developer", "Student", "Tutor"],
-  typedSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true
-});
